038a04b3ef3e288b5bffb19c98f2f31df9334dfb
nekoo <nereapena42@gmail.com>
2023-03-05 18:47:19 +0100

    creando nivel 2 junto con el pase de un nivel a otro

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private AudioSource finishSound;
+
+    //para comprobar si realmente hemos tocado la bandera por primera vez (y que no suene doble el sonido)
+    private bools levelCompleted = false;
+
+    private void Start()
+    {
+        finishSound = GetComponent<AudioSource>();
+    }
+
+   private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            finishSound.Play();
+            levelCompleted = true;   
+            Invoke("CompleteLevel", 2f)
+        }
+    }
+    private void CompleteLevel()
+    {
+        //pasamos a otra scena (+1)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
